c7fd6b84728f3bb07e11d04382a31266b97c0cd0
Alexander Jacks and Meaghan Jones <alexander+meaghan@>
2016-07-20 16:06:54 -0700

    begins building of front end

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/recipes") do
+  @recipes = Recipe.all()
+  erb(:new_recipe_form)
+end
+
+post("/recipes") do
+  name = params.fetch("recipe_name")
+  rank = params.fetch("rank")
+  instructions = params.fetch("instructions")
+  ingredient = []
+end
+
+post("/ingredients")
+  name = params.fetch("ingredient_name")
+  Ingredient.create({:name => name})
+  redirect("/recipes")
+end
---
+++ db/migrate/20160720214806_add_instructions_to_recipes.rb
@@ -0,0 +1,4 @@
+class AddInstructionsToRecipes < ActiveRecord::Migration
+  def change
+  end
+end
